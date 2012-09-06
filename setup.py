fc9711155969da0e450e0fc74b178a8a77a5b293
Peter Neumark <neumark.peter@gmail.com>
2012-09-06 17:22:14 +0200

    Got setup.py ready for the upcoming release

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='pip-rinstall',
-    version='0.0.1',
+    version='1.0.0',
     author_email='peter.neumark@prezi.com',
     packages=find_packages(),
     scripts=[],
     url='https://github.com/prezi/pip-rinstall',
     description='recursive requirements.txt processing for pip',
-    long_description='Please fill in this description.',
+    long_description='This is part of the example-project.',
     install_requires=['pip'],
 )
