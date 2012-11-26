f18302d68a71ca4073dab1e45ee38e0cdc33ebdd
Peter Neumark <peter.neumark@prezi.com>
2012-11-26 11:55:45 +0100

    removed tests

--- tests/tests_cache/django-authority/example/development.py
+++
@@ -1,4 +0,0 @@
-
-from example.settings import *
-DEBUG=True
-TEMPLATE_DEBUG=DEBUG
--- tests/tests_cache/django-authority/example/exampleapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from django.utils.translation import ugettext_lazy as _
-
-from authority.forms import UserPermissionForm
-
-class SpecialUserPermissionForm(UserPermissionForm):
-    user = forms.CharField(label=_('Special user'), widget=forms.Textarea())
