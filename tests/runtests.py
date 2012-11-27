34a77c1937ec36dcc034312c95351e67016d7ab7
Peter Neumark <peter.neumark@prezi.com>
2012-11-27 16:49:26 +0100

    remove unnecessary patch from runtests

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,4 +1,3 @@
-from nose.tools import set_trace;set_trace()
 import os
 import sys
 import xmlrpclib
@@ -16,31 +15,6 @@ mre = re.compile(".*pip.*")
 sys.modules = dict((k,v) for k,v in sys.modules.iteritems() if re.match(mre, k) is None)
 dre = re.compile(".*site-packages/pip-.*")
 sys.path = [d for d in sys.path if re.match(dre, d) is None]
-
-# patch assert_raises to allow always be true
-def patched_assert_raises_context_exit(self, exc_type, exc_value, tb):
-    if exc_type is None:
-        try:
-            exc_name = self.expected.__name__
-        except AttributeError:
-            exc_name = str(self.expected)
-        raise self.failureException(
-            "{0} not raised".format(exc_name))
-    if not (issubclass(exc_type, self.expected) or (exc_value is not None and exc_value.__class__.__name__ == self.expected.__name__)):
-        # let unexpected exceptions pass through
-        return False
-    self.exception = exc_value # store for later retrieval
-    if self.expected_regexp is None:
-        return True
-
-    expected_regexp = self.expected_regexp
-    if isinstance(expected_regexp, basestring):
-        expected_regexp = re.compile(expected_regexp)
-    if not expected_regexp.search(str(exc_value)):
-        raise self.failureException('"%s" does not match "%s"' %
-                 (expected_regexp.pattern, str(exc_value)))
-    return True
-sys.modules['unittest.case']._AssertRaisesContext.__exit__ = patched_assert_raises_context_exit
 
 import nose.selector
 def patched_getpackage(filename):
