ca4fec5485313a81c20787e1a8c1b8998aaedca1
Peter Neumark <peter.neumark@prezi.com>
2012-11-28 13:13:50 +0100

    pip tests now run by calling the sb script

--- setup.py
+++ setup.py
@@ -4,10 +4,9 @@ setup(
     name='snakebasket',
     version='1.0.0',
     author_email='peter.neumark@prezi.com',
-    packages=find_packages(),
-    scripts=['bin/sb'],
+    packages=find_packages(exclude=['tests']),
+    entry_points=dict(console_scripts=['sb=snakebasket:main']),
     url='https://github.com/prezi/snakebasket',
     description='Python environment and release management for prezi.',
-    long_description='This is part of the example-project.',
-    install_requires=['pip==1.1'],
+    long_description='This is part of the example-project.'
 )
