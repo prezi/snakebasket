f0666835018be5241931fdc29f227ed2688036b9
Peter Neumark <peter.neumark@prezi.com>
2012-11-28 15:49:40 +0100

    moved install command to parent dir

--- snakebasket/main.py
+++ snakebasket/main.py
@@ -2,12 +2,12 @@ import sys
 from pip import main as pip_main
 
 def main(*args, **kwargs):
-    #install_pip_patches()
+    install_pip_patches()
     return pip_main(*args, **kwargs)
 
 def install_pip_patches():
     from patches import patched_git_get_src_requirement
-    from snakebasket.commands.pip import install
+    from snakebasket.commands import install
     from snakebasket.commands import release
     import pip.vcs.git
     sys.modules['pip.commands.install'] = install
