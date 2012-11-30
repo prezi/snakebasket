3a15abccba502ba3b931855295bafc77badb7bbf
Peter Neumark <peter.neumark@prezi.com>
2012-11-30 14:46:42 +0100

    updated setup.py with the new version number.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='snakebasket',
-    version='1.0.0',
+    version='2.0.0',
     author_email='peter.neumark@prezi.com',
     packages=find_packages(exclude=['tests']),
     entry_points=dict(console_scripts=['sb=snakebasket:main']),
     url='https://github.com/prezi/snakebasket',
     description='Python environment and release management for prezi.',
-    long_description='This is part of the example-project.'
+    long_description='None yet'
 )
