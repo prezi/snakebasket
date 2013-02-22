efc56b2a9cdf9c0a4fea766a28515e7e9b940904
Ryan Lane <ryan.lane@prezi.com>
2013-02-22 12:50:09 +0100

    Removed one comment-out passing test from exclude list
    test_finder_priority_page_over_deplink

--- tests/runtests.py
+++ tests/runtests.py
@@ -44,7 +44,6 @@ if __name__ == '__main__':
         '-e', 'test_install_subversion_usersite_editable_with_distribute',
         '-e', 'test_freeze_bazaar_clone',
         # Temporarily excluded to get Jenkins job to pass (failed) 
-        # '-e', 'test_finder_priority_page_over_deplink',
         '-e', 'test_install_user_conflict_in_globalsite',
         '-e', 'test_install_user_conflict_in_globalsite_and_usersite',
         '-e', 'test_install_user_conflict_in_usersite',
