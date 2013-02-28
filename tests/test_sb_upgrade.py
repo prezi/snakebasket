1b6769537c04900d4d1bd435332c1d644c24ef31
Ryan Lane <ryan.lane@prezi.com>
2013-02-28 15:33:50 +0100

    Fixed test_no_upgrade_editable_if_prefer_pinned() to pass

--- tests/test_sb_upgrade.py
+++ tests/test_sb_upgrade.py
@@ -3,6 +3,7 @@ from tests.test_pip import (here, reset_env, run_pip, assert_all_changes,
                             write_file, pyversion, _create_test_package,
                             _change_test_package_version)
 from tests.local_repos import local_checkout
+import re
 
 def test_no_upgrade_pypi_if_prefer_pinned():
     """
@@ -16,7 +17,7 @@ def test_no_upgrade_pypi_if_prefer_pinned():
 
 def test_upgrade_pypi_if_no_prefer_pinned():
     """
-    Upgrade pypi package if 1)--prefer-pinned-revision is False and 2) previously installed version is pinned.
+    Upgrade pypi package if 1)--prefer-pinned-revision is False (default) and 2) previously installed version is pinned.
 
     """
     env = reset_env()
@@ -31,13 +32,31 @@ def test_no_upgrade_editable_if_prefer_pinned():
 
     """
     reset_env()
-    # run_pip('install', 'INITools==0.1', expect_error=True)
-    # result = run_pip('install','--prefer-pinned-revision', 'INITools', expect_error=True)
-    # assert not result.files_created, 'pip install INITools upgraded when it should not have'
+
+    local_url = local_checkout('git+http://github.com/prezi/sb-test-package.git')
+
+    args = ['install',
+        # older version
+        '-e', '%s@0.2.0#egg=sb-test-package' % local_url]
+
+    result = run_pip(*args, **{"expect_error": True})
+    result.assert_installed('sb-test-package')
+
+    args = ['install',
+        '--prefer-pinned-revision',
+        # unpinned newer version
+        '-e', '%s#egg=sb-test-package' % local_url]
+    result = run_pip(*args, **{"expect_error": True})
+
+    # worrysome_files_created are all files that aren't located in .git/, created by the comparison `git fetch`
+    expected_files_regex = re.compile('[.]git')
+    worrysome_files_created = [file_path for file_path in result.files_created.keys() if not expected_files_regex.search(file_path)]
+
+    assert not worrysome_files_created, 'sb install sb-test-package upgraded when it should not have'
 
 def test_upgrade_editable_if_no_prefer_pinned():
     """
-    Upgrade editable if 1)--prefer-pinned-revision is False and 2) previously installed version is pinned.
+    Upgrade editable if 1)--prefer-pinned-revision is False (default) and 2) previously installed version is pinned.
 
     """
     env = reset_env()
