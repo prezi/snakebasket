942b0e361214e19a2f41153ab38db435f2de7ce6
Daniel Torok <daniel.torok@prezi.com>
2014-04-18 11:02:32 +0200

    Revert "Merge pull request #13 from stanhu/master", since some tests are failing
    This reverts commit 006a7d0463577794397789a0ea3ae1ce5dd5310c, reversing
    changes made to 22a43c12047345c8724f803e4cc56a46af34846e.

--- tests/test_sb_upgrade.py
+++ tests/test_sb_upgrade.py
@@ -102,7 +102,7 @@ def test_no_upgrade_editable_if_uncommitted_change():
 
     # Make modification to an existing file
     with open(os.path.join(env.venv_path, 'src/sb-test-package', 'requirements.txt'), 'a') as file:
-        file.write('local modification!')
+        file.write('local modification!') 
 
     # Attempt to install a new version
     args = ['install',
@@ -142,37 +142,3 @@ def test_no_upgrade_editable_if_uncommitted_new_file():
     # worrysome_files_created = [file_path for file_path in result.files_created.keys() if not expected_files_regex.search(file_path)]
 
     # assert not worrysome_files_created, 'sb install sb-test-package upgraded when it should not have'
-
-def test_upgrade_editable_if_ignore_untracked_files():
-    """
-    Upgrade editable if --ignore-untracked-files is True
-
-    """
-    env = reset_env()
-
-    local_url = local_checkout('git+http://github.com/prezi/sb-test-package.git')
-
-    args = ['install',
-        # older version
-        '-e', '%s@0.2.0#egg=sb-test-package' % local_url]
-
-    result = run_pip(*args, **{"expect_error": True})
-    result.assert_installed('sb-test-package')
-
-    # Create a new file that isn't in source control
-    subprocess.Popen(['touch', 'new_file.txt'], cwd=os.path.join(env.venv_path, 'src/sb-test-package'), stdout=subprocess.PIPE)
-
-    # Attempt to install a new version
-    args = ['install',
-        '--ignore-untracked-files',
-        # unpinned newer version
-        '-e', '%s#egg=sb-test-package' % local_url]
-    result = run_pip(*args, **{"expect_error": True})
-
-    # worrysome_files_created are all files that aren't located in .git/, created by the comparison `git fetch`
-    expected_files_regex = re.compile('[.]git')
-    new_files_created = [file_path for file_path in result.files_created.keys() if not expected_files_regex.search(file_path)]
-
-    # new_files_created should contain a file that appears in versions >=0.2.1, but not in 0.2.2
-    assert new_files_created, 'sb install sb-test-package did not upgrade when it should have'
-
